9c438e57c97e36c37ad2dc930d98c9d49afd5fd8
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-08 07:26:09 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    return (0) ;
+}
