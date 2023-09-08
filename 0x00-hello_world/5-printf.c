de6f2f5c8f59b48aaa414402eeadd25103245611
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-07 20:05:15 -0700

    hiii

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - displays "with proper grammar" with new line
- * Return: 0 and exit the program
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
