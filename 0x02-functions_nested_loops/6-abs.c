649dd22add300c5e8fb11b562361325b31228999
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 10:44:09 -0700

    hello

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *print_sign - print_sign
+ * @n: The loweer to checked
+ * main - check the code.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else if (n < 0)
+	{
+	return (-(n));
+	}
+}
