8ad5a1d78f9e916eeffcd88d748a85518a650f3d
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 14:38:23 -0700

    hello

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 	int j;
 
 	if (n > 15 || n < 0)
-	return ;
+	return;
 
 	for (i = 0 ; i <= n ; i++)
 	{
@@ -22,11 +22,11 @@ void print_times_table(int n)
 	printf("%d", j * i);
 	else
 	{
-	printf(", %d", j * i);
+	printf(",  %d", j * i);
 	}
 	}
 	printf("\n");
 	}
 
-	return ;
+	return;
 }
