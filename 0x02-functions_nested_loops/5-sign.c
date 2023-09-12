fe83de0e1d0d176d61349d78d7c2edac816a76fd
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 10:22:06 -0700

    hello

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *print_sign - print_sign
+ * main - check the code.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+	printf("+")
+	return (1);
+	}
+	else if (n < '0')
+	{
+	printf("-")
+	return (-1);
+	}
+	else
+	{
+	printf("0")
+	return (0);
+	}
+}
