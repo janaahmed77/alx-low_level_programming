3183138996d84dade6bde4b8d025efacf41792ce
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 10:56:36 -0700

    hello

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *print_last_digit - print_last_digit
+ * @n: The loweer to checked
+ * main - check the code.
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	if (n >= 0)
+	{
+	_putchar(n + '0');
+	return (n % 10);
+	}
+	else
+	{
+	_putchar(n + '0')
+	return (-n % 10);
+	}
+}
