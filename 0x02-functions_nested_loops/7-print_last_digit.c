abdc3c5fa24baf40faddc346616b89f5d205998f
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 10:59:24 -0700

    hello

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@ int print_last_digit(int n)
 {
 	if (n >= 0)
 	{
-	_putchar(n + '0');
-	return (n % 10);
+	_putchar(n % 10 + '0');
+	return (n);
 	}
 	else
 	{
-	_putchar(n + '0');
-	return (-n % 10);
+	_putchar(n % 10 + '0');
+	return (n);
 	}
 }
