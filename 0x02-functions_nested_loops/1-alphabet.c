fffdcf91f9251113e5a5ebb02d00706e464d2185
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 08:22:40 -0700

    hello

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; 'a' <= 'z' ; alphabet++)
+	putchar(alphabet);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef Main
 #define Main
 int _putchar(char c);
+void print_alphabet(void);
 #endif /*main*/
