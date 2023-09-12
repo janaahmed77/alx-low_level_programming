f799cfa0a24d3335578447c7c127a28db3f54d70
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 12:46:13 -0700

    hello

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *add - add
+ * @a: The loweer to checked
+ * @b: The loweer to checked
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
