26dea6b8a39d8a56321482bb3a2bef9ac68dae4b
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-12 16:18:31 -0700

    hello

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int x1 = 1;
+	long int x2 = 2;
+	long int next;
+	long int sum = 0;
+
+	printf("%ld, %ld, ", x1, x2);
+	while (x1 <= 4000000)
+	{
+	if (x1 % 2 == 0)
+	sum = x1 + sum
+	next = x1 + x2;
+	x1 = x2;
+	x2 = next;
+	}
+	printf("%ld", sum);
+	return (0);
+}
