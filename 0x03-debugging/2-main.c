fe20fe2da6c95e0d54ba5d0e663aaf07bf92772a
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-13 16:39:43 -0700

    hiii

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -2,9 +2,12 @@
 #include "main.h"
 
 /**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
 int main(void)
 {
