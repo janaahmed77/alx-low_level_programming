42dea5291156ab5b0df912df02b49e7e61fc3650
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 13:34:50 -0700

    hi

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,12 @@ void print_most_numbers(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		if (i != 2 || != 4)
-		{
-		_putchar (i + '0');
-		}
+	if (c == 2 || c == 4)
+	{
+	continue;
+	}
+	_putchar (i + '0');
+
 	}
 	_putchar ('\n');
 }
