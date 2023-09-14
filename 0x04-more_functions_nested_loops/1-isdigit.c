fda677fb8cdb6db3c4a09ea392659482ee9b3033
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 12:03:50 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - _isdigit
+ *  * @c: the c is a digit
+ * Return: 1 if c is a digit else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c >= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
