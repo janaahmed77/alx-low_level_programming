ec42959d900c11619281a126ea16d2c6b002203f
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 12:14:26 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply a and b
+ *  * @a: mutliply
+ *  * @b: mutliply
+ * Return: 1 if c Upercase else 0
+ */
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	printf("%d", a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define Main
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif /*main*/
