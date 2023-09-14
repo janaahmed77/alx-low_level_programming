0e417288783475d7cf1fb7d5d507dbe928506743
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 14:05:35 -0700

    hi

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,15 @@ void more_numbers(void)
 	{
 	for (j = 0 ; j <= 14; j++)
 	{
-	if (j >= 10)
+	if (j > 9)
 	{
 	_putchar (j / 10 + '0');
+	}
+	if (j <= 14)
+	{
 	_putchar (j % 10 + '0');
 	}
-	_putchar ('\n');
 	}
+	_putchar ('\n');
 	}
 }
