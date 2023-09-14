575fbc2e314192b6e718cbdf3acfb455bc50bed4
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 14:38:41 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - return num without 2,4
+ *@n: n is underscore
+ * Return: Always
+ */
+void print_diagonal(int n)
+{
+int j;
+
+	if (n <= 0)
+	{
+	_putchar ('\n');
+	}
+	else
+	{
+	for (j = 0 ; j < n ; j++)
+	{
+	_putchar ('\\');
+	}
+	_putchar ('\n');
+	}
+}
