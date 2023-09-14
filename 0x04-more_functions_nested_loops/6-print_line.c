2ad1d5261bdb0f65702d14b84c3aa8ae9740df40
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 14:31:16 -0700

    hi

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,14 @@ void print_line(int n)
 	int j;
 	if (n <= 0)
 	{
-	_putchar ('\n')
+	_putchar ('\n');
 	}
 	else
 	{
 	for (j = 0 ; j < n ; j++)
 	{
-	_putchar ('_')
+	_putchar ('_');
 	}
-	_putchar ('\n')
+	_putchar ('\n');
 	}
 }
