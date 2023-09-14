e44365ace9495cf78c38c70b8cc767038c47089b
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 14:20:40 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - return num without 2,4
+ *@n: n is underscore
+ * Return: Always
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+	_putchar ('\n')
+	}
+	else
+	{
+		_putchar ('n')
+	}
+}
