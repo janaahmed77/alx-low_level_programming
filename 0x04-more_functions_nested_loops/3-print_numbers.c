9d014d9ccc4056edce18bb4e47a45aae6631b06d
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 12:37:53 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - _print_numbers
+ * Return: 1 if c is a digit else 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	_putchar (i + '0');
+}
