d4e33d9bd36a0e2ab46abc101da18acf9e455a6b
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-14 15:27:38 -0700

    hi

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int i;
+
 	for (i = 1 ; i <= 100 ; i++)
 	{
 	if (i % 3 == 0 && i % 5 == 0)
@@ -17,7 +18,7 @@ int main(void)
 	printf("Fizz");
 	else if (i % 5 == 0)
 	printf("Buzz");
-	else 
+	else
 	{
 	printf("%i", i);
 	}
