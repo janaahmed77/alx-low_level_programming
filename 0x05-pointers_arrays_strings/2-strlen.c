701f5acd3c6deb7d1c79710e064052f43d2c5846
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-18 14:37:27 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen - to swap value
+ * @s: The integer to be printed
+ * Return: Lenght
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+	length++;
+	s++;
+	}
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define Main
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif /*main*/
