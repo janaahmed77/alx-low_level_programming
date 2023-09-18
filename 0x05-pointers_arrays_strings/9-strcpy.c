060a2ba92b99893348a6a9d957b0690567bfa842
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-18 16:38:57 -0700

    hi

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,12 +3,13 @@
 
 /**
  * *_strcpy - Prints an integer
- * @scr: The integer to be printed
+ * @src: The integer to be printed
  * @dest: The integer to be printed
  * Return: last
  */
 char *_strcpy(char *dest, char *src)
 {
 	char *last = strcpy(dest, src);
+
 	return (last);
 }
