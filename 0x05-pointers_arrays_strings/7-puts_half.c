aa33079472eab1e427097e5b0e3f1bba8bfd1902
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-18 16:11:01 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - to swap value
+ * @str: The integer to be printed
+ */
+void puts_half(char *str)
+{
+	int lenght = strlen(str);
+	int i;
+
+	for (i = lenght / 2 ; i < lenght ; i++)
+	{
+		putchar(str[i]);
+	}
+	printf("\n");
+}
