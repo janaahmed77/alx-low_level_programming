4eae47e5d6a8095ab98c460c1cde08b62bdd42b1
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-25 15:17:52 -0700

    hi

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _memset - prints buffer
+ * @s: buffer
+ * @n: size
+ * @b:hii
+ * Return: void
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	void *memset(void *s, int n, size_t b);
+	return (*s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef Main
+#define Main
+char *_memset(char *s, char b, unsigned int n);
+#endif /*main*/
