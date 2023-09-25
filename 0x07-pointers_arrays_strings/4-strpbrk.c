01827feb31bb2ec0d2384e40fe90d6ecccbe9616
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-25 16:36:53 -0700

    hi

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strspn - concatenates two strings
+ * @s: char pointer
+ * @accept: char pointer
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept);
+{
+	char *Value = strspn(s, accept);
+
+	return (Value);
+}
