d1f9851bfc490bf6159d649bbfd6969f657b8faf
janaahmed77 <janaabdelmageed77@gmail.com>
2023-09-28 17:48:00 -0700

    hi

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion- function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+	else
+		_putchar('\n');
+}
